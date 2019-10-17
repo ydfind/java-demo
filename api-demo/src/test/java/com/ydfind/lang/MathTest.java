675a4a430ac636e2a1cb2dacaa290c7001dbe726
yd@ydfind.com <465628578@qq.com>
2019-10-17 11:07:37 +0800

    反正切atan及atan2

---
+++ api-demo/src/test/java/com/ydfind/lang/MathTest.java
@@ -0,0 +1,52 @@
+package com.ydfind.lang;
+
+import org.junit.Test;
+
+/**
+ * Math类测试
+ * @author ydfind
+ * @date 2019.10.17
+ */
+public class MathTest {
+
+    /**
+     * atan参数负无穷到正无穷，值-PI/2 到 PI/2；
+     * atan2参数为坐标yx，值为-PI 到 PI;
+     */
+    @Test
+    public void testTan(){
+        double atan2;
+        double angle;
+
+        // 45
+        atan2 = Math.atan2(1, 1);
+        angle = atan2 / Math.PI * 180;
+        System.out.println(angle + "," + atan2);
+        // 135
+        atan2 = Math.atan2(1, -1);
+        angle = atan2 / Math.PI * 180;
+        System.out.println(angle + "," + atan2);
+        // 225
+        atan2 = Math.atan2(-1, -1);
+        angle = atan2 / Math.PI * 180;
+        System.out.println(angle + "," + atan2);
+        // 315
+        atan2 = Math.atan2(-1, 1);
+        angle = atan2 / Math.PI * 180;
+        System.out.println(angle + "," + atan2);
+
+        double atan;
+        // 0
+        atan = Math.atan(0);
+        angle = atan / Math.PI * 180;
+        System.out.println(angle + "," + atan);
+        // 1
+        atan = Math.atan(1);
+        angle = atan / Math.PI * 180;
+        System.out.println(angle + "," + atan);
+        // -1
+        atan = Math.atan(-1);
+        angle = atan / Math.PI * 180;
+        System.out.println(angle + "," + atan);
+    }
+}
